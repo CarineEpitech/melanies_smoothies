943d4a9788fcf5d955565f2e69cfe9ea50d9a908
Carine AGBOTON <carineagboton268@gmail.com>
2026-08-27 19:22:37 +0100

    fix streamlit_app.py
    Add quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                    values ('""" + ingredients_string + """','"
+                    values ('""" + ingredients_string + """',"""
 
     #st.write(my_insert_stmt)
     #st.stop()
