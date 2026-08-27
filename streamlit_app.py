2800b49671538de4dc9bede18f48ff38190b0767
Carine AGBOTON <carineagboton268@gmail.com>
2026-08-27 19:13:11 +0100

    fix streamlit_app.py
    used my_dataframe as pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:'
-    ,my_dataframe
+    ,pd_df
     ,max_selections=5)
 
 if ingredients_list:
@@ -33,7 +33,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
